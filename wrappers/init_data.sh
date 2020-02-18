92182c9278cc881bbcc06e67bc81e7fae9a2ff51
Daniel Juravski <juravski95@gmail.com>
2020-02-18 13:04:34 +0200

    support logs dir arg

--- wrappers/init_data.sh
+++ wrappers/init_data.sh
@@ -3,16 +3,18 @@
 TWEETS_TEXT_FILE=$1
 VOCAB_FILE=$2
 DATA_PKL_FILE=$3
+LOGS_DIR=$4
 
 [[ ! -z "$TWEETS_TEXT_FILE" ]] && echo "TWEETS_TEXT_FILE: $TWEETS_TEXT_FILE" || echo "[WARNING] TWEETS_TEXT_FILE arg empty"
 [[ ! -z "$VOCAB_FILE" ]] && echo "VOCAB_FILE: $VOCAB_FILE" || echo "[WARNING] VOCAB_FILE arg empty"
 [[ ! -z "$DATA_PKL_FILE" ]] && echo "DATA_PKL_FILE: $DATA_PKL_FILE" || echo "[WARNING] DATA_PKL_FILE arg empty"
+[[ ! -z "$LOGS_DIR" ]] && echo "LOGS_DIR: $LOGS_DIR" || echo "[WARNING] LOGS_DIR arg empty" && LOGS_DIR='/home/daniel/heMoji/logs'
 
 
 echo Start making vocab file
-python src/make_vocab.py $TWEETS_TEXT_FILE $VOCAB_FILE &> logs/vocab.log
+python src/make_vocab.py $TWEETS_TEXT_FILE $VOCAB_FILE &> $LOGS_DIR/vocab.log
 echo Done making vocab file
 
 echo Start making dataset pkl file
-python src/make_dataset.py $TWEETS_TEXT_FILE $DATA_PKL_FILE &> logs/dataset.log
+python src/make_dataset.py $TWEETS_TEXT_FILE $DATA_PKL_FILE &> $LOGS_DIR/dataset.log
 echo Done making dataset pkl file
