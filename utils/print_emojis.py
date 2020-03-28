5c279d0a4d02317b5eac8a6fbd9c9f9040e05215
Daniel Juravski <juravski95@gmail.com>
2020-03-28 15:19:02 +0300

    add data01 emojis file

--- utils/print_emojis.py
+++ utils/print_emojis.py
@@ -1,8 +1,18 @@
-from src.emoji2label import l2edata01 as l2e
+"""
+PRINTING EMOJIS FROM THE DESIRED DATA_TYPE TO FILE (emojis/DATA_TYPE.txt)
+"""
+DATA_TYPE = 'all'
 
-with open('/utils/emojis.txt', 'w') as f:
-    for e in l2e:
-        e_unicode = l2e[e]
-        line = str(e) + '\t' + '\t' + e_unicode.encode('utf-8')
+
+l2e_str = 'l2e' + DATA_TYPE
+# exec "from src.emoji2label import %s as e2l" % e2l_str
+exec "from src.emoji2label import %s as l2e" % l2e_str
+
+
+file_path = 'emojis/{0}.txt'.format(DATA_TYPE)
+with open(file_path, 'w') as f:
+    for l in l2e:
+        e_unicode = l2e[l]
+        line = str(l) + '\t' + e_unicode.encode('utf-8')
         f.writelines(line)
         f.writelines('\n')
