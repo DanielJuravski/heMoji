217620c74c2ad51bc973f5f0e10efe472c4a6a18
Daniel Juravski <juravski95@gmail.com>
2020-04-14 09:48:16 +0300

    add beuty emoji prob plot

--- fe/app.py
+++ fe/app.py
@@ -3,6 +3,11 @@ from keras.models import load_model
 import json
 import numpy as np
 from keras import backend as K
+import matplotlib.pyplot as plt
+import pandas as pd
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 from lib.sentence_tokenizer import SentenceTokenizer
 from lib.attlayer import AttentionWeightedAverage
@@ -16,7 +21,9 @@ e2l_str = data + "e2l"
 l2e_str = "l2e" + data
 exec("from src.emoji2label import %s as e2l") % e2l_str
 exec("from src.emoji2label import %s as l2e") % l2e_str
-TOP_E = len(e2l)
+l2e = l2e  # let's have the 'Unresolved' error once and last here
+e2l = e2l  # let's have the 'Unresolved' error once and last here
+TOP_E = 5  # len(e2l)
 
 
 @st.cache(allow_output_mutation=True)
@@ -42,50 +49,76 @@ def load_my_model():
     return model, session
 
 
-@st.cache(allow_output_mutation=False)
-def load_sentok(vocab):
-    with open(conf['vocab'], 'r') as f:
-        vocab1 = json.load(f)
-    sentok = SentenceTokenizer(vocab1, maxlen, prod=True, wanted_emojis=e2l, uint=32)
+def design_cells(val):
+    """
+    design the result DF that the first line (eomjis) will be large
+    and the second line (probs) will be green color
+    :param val:
+    :return:
+    """
+    # ugly way to get if it is emoji or prob-val
+    if val[0][0] != ".":
+        # emoji
+        return ['font-size:20pt'] * TOP_E
+    else:
+        # prob
+        return ['color: green'] * TOP_E
 
-    return sentok
+
+def edit_probs(result):
+    """
+    1. round to 4 digits after dot
+    2. remove leading zero
+    0.1234567 --> .1234
+    :param result:
+    :return:
+    """
+    for i in range(TOP_E):
+        p = result.data[i][1]
+        p = round(p, 4)
+        p_str = str(p)[1:]
+        result.data[i][1] = p_str
+
+    return result
 
 
 if __name__ == '__main__':
     """
-    $ streamlit --log_level debug run hello.py
+    some pretty UI that loads the model and predicts emoji based on text
     """
     vocab = load_my_vocab()
     sentok = SentenceTokenizer(vocab, maxlen, prod=True, wanted_emojis=e2l, uint=32)
-    # sentok = load_sentok(vocab)
     model, session = load_my_model()
 
-    st.title('My first app')
-    st.write('Insert text:')
+    st.sidebar.title("Mode")
+    mode = st.sidebar.radio(label="", options=["Basic", "Advanced"])
 
-    sentence = st.text_input('Input your sentence here:')
+    st.title('***heMoji*** Predictor')
+    st.subheader('***heMoji*** will try to understand the sentiment of your Hebrew sentence and predict the correspond emoji for it')
+
+    sentence = st.text_input('Insert Hebrew phrase:')
     if sentence:
-        st.write(sentence)
-
+        # encode sentence to tokens
         u_line = [sentence]
         tokens, infos, stats = sentok.tokenize_sentences(u_line)
-        st.write(tokens)
+        if mode == "Advanced":
+            st.write("Input tokens:")
+            st.write(tokens)
 
         K.set_session(session)
         e_scores = model.predict(tokens)[0]  # there is only 1 macro array since it is the return of the softmax layer
         e_labels = np.argsort(e_scores)  # sort: min --> max
-
         e_labels_reverse = e_labels[::-1]  # reverse max --> min
         e_labels_reverse_scores = [e_scores[i] for i in e_labels_reverse]  # prob of every label
         e_top_labels = e_labels_reverse[:TOP_E]  # top
+        e_top = [l2e[e] for e in e_top_labels]
         e_top_labels_scores = e_labels_reverse_scores[:TOP_E]  # top
 
-        for e, score in zip(e_top_labels, e_top_labels_scores):
-            e_unicode = l2e[e]
-            line = e_unicode.encode('utf-8') + '\t' + '(' + str(score) + ')'
-            st.write(e_unicode)
+        result = pd.DataFrame({'emoji': e_top, 'prob': e_top_labels_scores}).T
+        result = result.style.apply(design_cells, axis=1)
+        result = edit_probs(result)
+        st.table(result)
 
-
-        print("Done!\n")
+        print("Predicted!\n")
 
 
