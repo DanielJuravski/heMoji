41d719adc11045433147b674e0021aa2a758acd8
Daniel Juravski <juravski95@gmail.com>
2020-04-19 18:13:25 +0300

    refactor src to class usage

--- fe/heMoji.py
+++ fe/heMoji.py
@@ -33,6 +33,163 @@ HOME = expanduser("~")
 LOGGER_PATH = HOME + '/emoji_predictor.log'
 
 
+class EmojiUI:
+    def __init__(self, model, session, sentok):
+        self.model = model
+        self.session = session
+        self.sentok = sentok
+
+    def home_page(self):
+        # tittles
+        st.balloons()
+        st.title('***heMoji*** Predictor')
+        st.subheader('***heMoji*** will try to detect the sentiment, emotion and sarcasm of your Hebrew sentence and predict the correspond emoji for it')
+
+        # user input
+        input_sentence_str, input_sentence_warning_str = self.get_input_sentence()
+        if input_sentence_warning_str:
+            self.w_input_sentence_warning = st.warning(input_sentence_warning_str)
+        self.w_input_sentence_error = st.empty()
+
+        # result table
+        self.w_result_table_text = st.empty()
+        self.w_result_table = st.empty()
+
+        # example sentences
+        st.write("<p style='font-size:80%;'>Try to click on any example sentence below:</p>", unsafe_allow_html=True)
+        example_sentence_str = self.use_example_sentence()
+        if example_sentence_str:
+            self.w_input_sentence_text.text_input('Insert Hebrew sentence:', value=example_sentence_str, key=0)
+
+        # a nice BIU logo
+        image = Image.open('biu_logo_transparent.png')
+        st.image(image, width=100)
+
+        return input_sentence_str, example_sentence_str
+
+    def get_input_sentence(self):
+        input_sentence_warning_str = None
+        # user input sentence
+        self.w_input_sentence_text = st.empty()
+        # set sentence_widget location to the right
+        st.markdown("""<style>input {direction: RTL;}</style>""", unsafe_allow_html=True)
+        input_sentence_str = self.w_input_sentence_text.text_input('Insert Hebrew sentence:', key=0)
+
+        if str_contains_en_chars(input_sentence_str):
+            input_sentence_warning_str = "Your sentence contains non Hebrew characters, which the predictor doesn't support"
+
+        return input_sentence_str, input_sentence_warning_str
+
+    def use_example_sentence(self):
+        # set buttons location to the right
+        st.markdown("""
+                <style>
+                .stButton>button {
+                    unicode-bidi:bidi-override;
+                    direction: RTL;
+                    display: block;
+                    margin-left: auto;}
+                </style>
+                    """, unsafe_allow_html=True)
+        with open('examples.json') as f:
+            conf = json.load(f, encoding='utf-8')
+            s1 = conf['s1']
+            s2 = conf['s2']
+            s3 = conf['s3']
+            s4 = conf['s4']
+            s5 = conf['s5']
+            s6 = conf['s6']
+            s7 = conf['s7']
+
+        b1 = st.button(s1)
+        b2 = st.button(s2)
+        b3 = st.button(s3)
+        b4 = st.button(s4)
+        b5 = st.button(s5)
+        b6 = st.button(s6)
+        b7 = st.button(s7)
+
+        if b1:
+            return s1
+        elif b2:
+            return s2
+        elif b3:
+            return s3
+        elif b4:
+            return s4
+        elif b5:
+            return s5
+        elif b6:
+            return s6
+        elif b7:
+            return s7
+        else:
+            return None
+
+    def predict(self, input_sentence_str, example_sentence_str):
+        sentence_str = None
+        log_sentence = None
+
+        # user input sentence
+        if input_sentence_str:
+            sentence_str = input_sentence_str
+            log_sentence = True
+
+        # state = SessionState.get(key=0)
+        if example_sentence_str is not None:
+            # state.key += 1
+            sentence_str = example_sentence_str
+            log_sentence = False
+
+        if sentence_str:
+            tokens = self.encode_input_sentence(sentence_str)
+            if tokens is not None:
+                result, log_result = self.evaluate_input_sentence(tokens)
+                result_table = style_result(result)
+
+                # display emoji predictions
+                self.w_result_table_text.markdown("<p style='font-size:80%;'>Predicted emojis:</p>", unsafe_allow_html=True)
+                self.w_result_table.table(result_table)
+
+                if log_sentence:
+                    logger(sentence_str, tokens, log_result)
+
+    def encode_input_sentence(self, input_sentence):
+        # encode sentence to tokens
+        u_line = [input_sentence]
+        try:
+            tokens, infos, stats = self.sentok.tokenize_sentences(u_line)
+        except AssertionError as e:
+            self.w_input_sentence_error.error("I think you've entered invalid sentence, please try another sentence.")
+            tokens = None
+
+        return tokens
+
+    def evaluate_input_sentence(self, tokens):
+        K.set_session(self.session)
+        e_scores = self.model.predict(tokens)[0]  # there is only 1 macro array since it is the return of the softmax layer
+        e_labels = np.argsort(e_scores)  # sort: min --> max
+        e_labels_reverse = e_labels[::-1]  # reverse max --> min
+        e_labels_reverse_scores = [e_scores[i] for i in e_labels_reverse]  # prob of every label
+        emojis = [l2e[e] for e in e_labels_reverse]
+        e_top_labels = e_labels_reverse[:TOP_E]  # top
+        emojis_top = [l2e[e] for e in e_top_labels]
+        e_top_labels_scores = e_labels_reverse_scores[:TOP_E]  # top
+
+        result = pd.DataFrame({'emoji': emojis_top, 'prob': e_top_labels_scores}).T
+        log_result = pd.DataFrame({'emoji': emojis, 'emoji_label': e_labels_reverse, 'prob': e_labels_reverse_scores})
+
+        return result, log_result
+
+
+def loaders():
+    vocab = load_my_vocab()
+    sentok = SentenceTokenizer(vocab, maxlen, prod=True, wanted_emojis=e2l, uint=32)
+    model, session = load_heMoji_model()
+
+    return model, session, sentok
+
+
 @st.cache(allow_output_mutation=True)
 def load_my_vocab():
     with open(conf['vocab'], 'r') as f:
@@ -89,64 +246,9 @@ def edit_probs(result):
     return result
 
 
-def loaders():
-    vocab = load_my_vocab()
-    sentok = SentenceTokenizer(vocab, maxlen, prod=True, wanted_emojis=e2l, uint=32)
-    model, session = load_heMoji_model()
-
-    return model, session, sentok
-
-
-def use_example_sentence():
-    # set buttons location to the right
-    st.markdown("""
-            <style>
-            .stButton>button {
-                unicode-bidi:bidi-override;
-                direction: RTL;
-                display: block;
-                margin-left: auto;}
-            </style>
-                """, unsafe_allow_html=True)
-    with open('examples.json') as f:
-        conf = json.load(f, encoding='utf-8')
-        s1 = conf['s1']
-        s2 = conf['s2']
-        s3 = conf['s3']
-        s4 = conf['s4']
-        s5 = conf['s5']
-        s6 = conf['s6']
-        s7 = conf['s7']
-
-    b1 = st.button(s1)
-    b2 = st.button(s2)
-    b3 = st.button(s3)
-    b4 = st.button(s4)
-    b5 = st.button(s5)
-    b6 = st.button(s6)
-    b7 = st.button(s7)
-
-    if b1:
-        return s1
-    elif b2:
-        return s2
-    elif b3:
-        return s3
-    elif b4:
-        return s4
-    elif b5:
-        return s5
-    elif b6:
-        return s6
-    elif b7:
-        return s7
-    else:
-        return None
-
-
-def sentence_contains_en_chars(sentence_str):
+def str_contains_en_chars(str):
     contains = False
-    for i in sentence_str:
+    for i in str:
         if i in string.lowercase:
             contains = True
             break
@@ -157,77 +259,11 @@ def sentence_contains_en_chars(sentence_str):
     return contains
 
 
-def get_input_sentence():
-    # user input sentence
-    sentence_widget = st.empty()
-    # set sentence_widget location to the right
-    st.markdown("""<style>input {direction: RTL;}</style>""", unsafe_allow_html=True)
-    sentence_str = sentence_widget.text_input('Insert Hebrew sentence:', key=0)
-    sentence_widget_warning = st.empty()
-
-    if sentence_contains_en_chars(sentence_str):
-        sentence_widget_warning.warning("Your sentence contains non Hebrew characters, which the predictor doesn't support")
-
-    return sentence_str
-
-
-def home_page():
-    st.balloons()
-    st.title('***heMoji*** Predictor')
-    st.subheader('***heMoji*** will try to detect the sentiment, emotion and sarcasm of your Hebrew sentence and predict the correspond emoji for it')
-
-    st.sidebar.title("Mode")
-    mode = st.sidebar.radio(label="", options=["Basic", "Advanced"])
-    st.sidebar.markdown("")
-    st.sidebar.markdown("")
-    st.sidebar.markdown("")
-    st.sidebar.markdown("")
-    st.sidebar.markdown("")
-    image = Image.open('biu_logo_transparent.png')
-    st.sidebar.image(image, width=150)
-
-    sentence = get_input_sentence()
-
-    result_table_text_w = st.empty()
-    result_table_w = st.empty()
-
-    return mode, sentence, result_table_text_w, result_table_w
-
-
-def predict_input_sentence(session, tokens):
-    K.set_session(session)
-    e_scores = model.predict(tokens)[0]  # there is only 1 macro array since it is the return of the softmax layer
-    e_labels = np.argsort(e_scores)  # sort: min --> max
-    e_labels_reverse = e_labels[::-1]  # reverse max --> min
-    e_labels_reverse_scores = [e_scores[i] for i in e_labels_reverse]  # prob of every label
-    emojis = [l2e[e] for e in e_labels_reverse]
-    e_top_labels = e_labels_reverse[:TOP_E]  # top
-    emojis_top = [l2e[e] for e in e_top_labels]
-    e_top_labels_scores = e_labels_reverse_scores[:TOP_E]  # top
-
-    result = pd.DataFrame({'emoji': emojis_top, 'prob': e_top_labels_scores}).T
-    log_result = pd.DataFrame({'emoji': emojis, 'emoji_label': e_labels_reverse, 'prob': e_labels_reverse_scores})
-
-    return result, log_result
-
-
 def style_result(result):
     result = result.style.apply(design_cells, axis=1)
     result = edit_probs(result)
 
     return result
-
-
-def encode_input_sentence(input_sentence):
-    # encode sentence to tokens
-    u_line = [input_sentence]
-    try:
-        tokens, infos, stats = sentok.tokenize_sentences(u_line)
-    except AssertionError as e:
-        st.error("I think you've entered invalid sentence, please try another sentence.")
-        tokens = None
-
-    return tokens
 
 
 def logger(input_sentence, tokens, log_result):
@@ -249,61 +285,6 @@ if __name__ == '__main__':
     """
     model, session, sentok = loaders()
 
-    mode, input_sentence, result_table_text_w, result_table_w = home_page()
-
-    if input_sentence:
-        tokens = encode_input_sentence(input_sentence)
-        if tokens is not None:
-            # display sentence tokens
-            if mode == "Advanced":
-                st.write("Input tokens:")
-                st.write(tokens)
-            result, log_result = predict_input_sentence(session, tokens)
-            result_table = style_result(result)
-
-            # display emoji predictions
-            result_table_text_w.markdown("<p style='font-size:80%;'>Predicted emojis:</p>", unsafe_allow_html=True)
-            result_table_w.table(result_table)
-
-            # log session
-            logger(input_sentence, tokens, log_result)
-
-    # example sentences
-    st.write("<p style='font-size:80%;'>Or click on any example sentence below:</p>", unsafe_allow_html=True)
-    example_sentence = use_example_sentence()
-    # state = SessionState.get(key=0)
-    if example_sentence is not None:
-        # state.key += 1
-        sentence_str = example_sentence
-        tokens = encode_input_sentence(sentence_str)
-        # sentence_str = sentence_widget.text_input('Insert Hebrew sentence:', value=example_sentence, key=0)
-        result, log_result = predict_input_sentence(session, tokens)
-        result_table = style_result(result)
-
-        # display emoji predictions
-        result_table_text_w.markdown("<p style='font-size:80%;'>Predicted emojis:</p>", unsafe_allow_html=True)
-        result_table_w.table(result_table)
-
-    # take care transcription json input file
-    if mode == "Advanced":
-        uploaded_file = st.file_uploader("Choose a transcription json file")
-        if uploaded_file is not None:
-            trans = json.load(uploaded_file)
-            for turn in trans["dialog_turns_list"]:
-                if turn["speaker"] == "Client":
-                    for mini_turn in turn["mini_dialog_turn_list"]:
-                        if mini_turn["speaker"] == "Client":
-                            st.markdown("------------")
-                            input_sentence = mini_turn["plainText"]
-                            st.write("<p style='font-size:80%;'>Input sentence:</p>", unsafe_allow_html=True)
-                            st.write(input_sentence)
-                            tokens = encode_input_sentence(input_sentence)
-                            if tokens is not None:
-                                result, log_result = predict_input_sentence(session, tokens)
-                                result_table = style_result(result)
-                                # display emoji predictions
-                                st.write("<p style='font-size:80%;'>Predicted emojis:</p>", unsafe_allow_html=True)
-                                st.table(result_table)
-
-
-
+    eui = EmojiUI(model, session, sentok)
+    input_sentence_str, example_sentence_str = eui.home_page()
+    eui.predict(input_sentence_str, example_sentence_str)
