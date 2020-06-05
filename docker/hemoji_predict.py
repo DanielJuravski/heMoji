7f61e7d2eff5189969aa377656deeb37d6cf39d5
Daniel Juravski <juravski95@gmail.com>
2020-06-05 16:45:41 +0300

    redesign functions structure

--- docker/hemoji_predict.py
+++ docker/hemoji_predict.py
@@ -1,5 +1,6 @@
 import json
 from keras.models import load_model
+import tensorflow as tf
 import numpy as np
 from tqdm import trange
 import argparse
@@ -41,7 +42,7 @@ def encode_input_sentence(sentok, input_sentence):
     return tokens
 
 
-def evaluate(model, tokens, top_k=5):
+def evaluate(model, tokens):
     """
     evaluate the model based on the given tokens
     :param model:
@@ -49,24 +50,22 @@ def evaluate(model, tokens, top_k=5):
     :param top_k: number of emojis to print for short output
     :return:
     """
-    e_scores = model.predict(tokens)[0]
-    e_labels = np.argsort(e_scores)
-    e_labels_reverse = e_labels[::-1]
-    e_labels_reverse_probs = [e_scores[i] for i in e_labels_reverse]
-    emojis = [l2e[e].decode('utf-8') for e in e_labels_reverse]
-    e_top_labels = e_labels_reverse[:top_k]  # top
-    e_top_labels_scores = e_labels_reverse_probs[:top_k]  # top
-    top_emojis = emojis[:top_k]
+    with graph.as_default():
+        e_scores = model.predict(tokens)[0]
+        e_labels = np.argsort(e_scores)
+        e_labels_reverse = e_labels[::-1]
+        e_labels_reverse_probs = ["%.32f" % e_scores[i] for i in e_labels_reverse]
+        emojis = [l2e[e].decode('utf-8') for e in e_labels_reverse]
 
-    return emojis, top_emojis, e_labels_reverse_probs
+    return emojis, e_labels_reverse_probs
 
 
-def dump_results(line, emojis, emojis_probs, top_emojis):
+def dump_results(out_path, line, emojis, emojis_probs, top_e=5):
     full_out = ({'input': line,
                  'emojis': str(emojis),
-                 'probs': str(emojis_probs),})
+                 'probs': str(emojis_probs)})
 
-    short_out = top_emojis
+    short_out = emojis[:top_e]
 
     # full out
     with open(out_path + 'out.json', 'a') as f:
@@ -89,10 +88,10 @@ def init_out_files(out_path):
         pass
 
 
-if __name__ == '__main__':
-    data_path, out_path = get_args()
+def load_hemoji_model():
+    global graph
+    graph = tf.get_default_graph()
 
-    print("Loading model ...")
     with open(VOCAB_PATH, 'r') as f:
         vocab = json.load(f)
 
@@ -100,27 +99,37 @@ if __name__ == '__main__':
 
     model = load_model(PRETRAINED_PATH, custom_objects={'AttentionWeightedAverage': AttentionWeightedAverage})
 
+    return sentok, model
+
+
+def main():
+    data_path, out_path = get_args()
+
+    print("Loading model ...")
+    sentok, model = load_hemoji_model()
+
+    # init output files
     init_out_files(out_path)
 
+    # iterate over input data file
+    print("Predicting text data from {} ...".format(data_path))
     with open(data_path, 'r') as f:
-        print("Predicting text data from {} ...".format(data_path))
-
         lines = f.readlines()
         n_sents = len(lines)
-
         for i in trange(n_sents):
             line = lines[i]
             line = line.strip()
             tokens = encode_input_sentence(sentok, input_sentence=line)
             if tokens is not None:
-                emojis, top_emojis, emojis_probs = evaluate(model, tokens)
-                dump_results(line, emojis, emojis_probs, top_emojis)
+                emojis, emojis_probs = evaluate(model, tokens)
+                dump_results(out_path, line, emojis, emojis_probs)
             else:
-                dump_results(line, 'N/A', 'N/A', 'N/A')
+                dump_results(out_path, line, 'N/A', 'N/A')
 
     print("Results were dumped to {}".format(out_path))
 
     print("Successfully Done !")
 
 
-
+if __name__ == '__main__':
+    main()
